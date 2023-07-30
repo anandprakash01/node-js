222b1013a719a4a860482c06a42262a5a1c8a2ba
Anand <hr.anand0101@gmail.com>
2023-07-31 01:27:23 +0530

    adding files of http module:)

---
+++ 4-http/module.js
@@ -0,0 +1,10 @@
+const add = (a, b) => a + b;
+
+const substract = (a, b) => a - b;
+
+const functions = {
+  add,
+  substract,
+};
+
+module.exports = functions;
---
+++ 4-http/testModule.js
@@ -0,0 +1,5 @@
+const func = require("4-http");
+
+console.log(func.add(2, 3));
+
+console.log(func.substract(3, 3));
